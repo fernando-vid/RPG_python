5fa81323ed8383dcfc7f14caa03f75c5f6f38034
Fernando Vidal <90697518+fernando-vid@users.noreply.github.com>
2021-09-14 17:16:14 -0300

    Foi adicionado a sala 8

--- script.py
+++ script.py
@@ -29,7 +29,11 @@ def Escolhercaminho():
         SalaAtual += 2
     else:
         print("Erro nao existe esse caminho")
-  
+    
+    if (SalaAtual == 8):
+      print("Voce caiu em um portal portanto vc vai retornar para uma sala aleatoria")
+      SalaAtual = random.randint(1, 5)
+      print("Voce retornou para a sala {}".format(SalaAtual))
 
 while (salaAtual < 9 and not(perdeu)):
   Escolhercaminho()
